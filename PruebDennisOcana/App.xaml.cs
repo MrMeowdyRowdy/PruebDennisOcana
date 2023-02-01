95e9f11523b217cc34d745f59406b5b74044f642
laboratorio <laboratorio@UPEAULA-31208>
2023-02-01 08:17:49 -0500

    Agregar archivos de proyecto.

---
+++ PruebDennisOcana/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PruebDennisOcana;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ PruebDennisOcana/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PruebDennisOcana;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
